c26e714ced90e7571a5a0fde9911450ada872860
Matt Dick <mattwd7@gmail.com>
2022-02-24 11:21:19 -0800

    New Service generator

---
+++ lib/generators/service_objects_rails/new_generator.rb
@@ -0,0 +1,38 @@
+require "rails/generators"
+
+module ServiceObjectsRails
+  module Generators
+    class NewGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __dir__)
+
+      argument :service_name, type: :string
+      argument :service_args, type: :array, default: []
+
+      desc "Creates a new Service Object and corresponding spec file"
+
+      def create_new_service
+        template(
+          "new_service.erb",
+          "#{Rails.root}/app/services/#{service_filepath}.rb"
+        )
+      end
+
+      def create_new_service_spec
+        template(
+          "new_service_spec.erb",
+          "#{Rails.root}/spec/services/#{service_filepath}_spec.rb"
+        )
+      end
+
+      private
+
+      def service_filepath
+        service_name.gsub(/::/, '/')
+          .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+          .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+          .tr("-", "_")
+          .downcase
+      end
+    end
+  end
+end
---
+++ spec/lib/generators/service_objects_rails/new_generator_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+require "generators/service_objects_rails/new_generator"
+
+describe ServiceObjectsRails::Generators::NewGenerator do
+  let(:service_name) { "MyNewService" }
+  let(:service_path) { "#{Rails.root}/app/services/my_new_service.rb" }
+  let(:service_spec_path) { "#{Rails.root}/spec/services/my_new_service_spec.rb" }
+
+  before { remove_files_and_directories }
+  after { remove_files_and_directories }
+
+  it "creates a new service object in the app/services directory" do
+    described_class.start([service_name])
+
+    file_contents = File.read(service_path)
+    expect(file_contents).to include("class MyNewService")
+  end
+
+  it "creates a new service object in the app/services directory" do
+    described_class.start([service_name])
+
+    file_contents = File.read(service_spec_path)
+    expect(file_contents).to include("RSpec.describe Services::MyNewService do")
+  end
+
+  it "creates a service response subject that calls the service" do
+    described_class.start([service_name, "user_id", "friend_id"])
+
+    file_contents = File.read(service_spec_path)
+    expect(file_contents).to include("subject(:service_response)")
+    expect(file_contents).to include("described_class.call")
+  end
+
+  context "with service arguments" do
+    it "creates a service object that accepts provided service arguments" do
+      described_class.start([service_name, "user_id", "friend_id"])
+
+      file_contents = File.read(service_path)
+      expect(file_contents).to include("def initialize(user_id:, friend_id:)")
+    end
+
+    it "makes service arguments privately readable" do
+      described_class.start([service_name, "user_id", "friend_id"])
+
+      file_contents = File.read(service_path)
+      expect(file_contents).to include("attr_reader :user_id, :friend_id")
+    end
+
+    it "creates a spec with service response subject that calls the service with arguments" do
+      described_class.start([service_name, "user_id", "friend_id"])
+
+      file_contents = File.read(service_spec_path)
+      expect(file_contents).to include("described_class.call(\n")
+      expect(file_contents).to include("user_id: user_id,\n")
+      expect(file_contents).to include("friend_id: friend_id\n")
+    end
+
+    it "creates a spec with `let`s defined for each argument" do
+      described_class.start([service_name, "user_id", "friend_id"])
+
+      file_contents = File.read(service_spec_path)
+      expect(file_contents).to include("let(:user_id) { nil }")
+      expect(file_contents).to include("let(:friend_id) { nil }")
+    end
+  end
+
+  context "with namespacing" do
+    let(:service_name) { "Users::MyNewService" }
+    let(:service_path) { "#{Rails.root}/app/services/users/my_new_service.rb" }
+
+    it "creates a new service object within an appropriate file structure" do
+      described_class.start([service_name])
+
+      file_contents = File.read(service_path)
+      expect(file_contents).to include("class Users::MyNewService")
+    end
+  end
+end
